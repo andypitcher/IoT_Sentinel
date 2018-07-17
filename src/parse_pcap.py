04afe2063463c2414cabf7cc1fe73f6707248965
andyp <andyp@localhost.localdomain>
2018-07-17 00:22:45 -0400

    v1.0 dir cleaning - ip_dst_counter OK

--- src/parse_pcap.py
+++ src/parse_pcap.py
@@ -17,26 +17,22 @@ import glob, os
     f= number of new destination ip address
 
     f= port class ref 0 or 1 0r 2 or 3
-
-        no port => 0
-        well known port [0,1023] => 1
-        registered port [1024,49151] => 2
-        dynamic port [49152,65535] => 3
         
 """
 
 def ip_to_str(address):
     """Print out an IP address given a string
-
-    Args:
-        address (inet struct): inet network address
-    Returns:
-        str: Printable/readable IP address
     """
     return socket.inet_ntop(socket.AF_INET, address)
 
 
 def port_class_def(ip_port):
+    """
+        no port => 0
+        well known port [0,1023] => 1
+        registered port [1024,49151] => 2
+        dynamic port [49152,65535] => 3
+    """
 
     if 0 <= ip_port <= 1023:
         return 1
@@ -47,18 +43,26 @@ def port_class_def(ip_port):
     else:
         return 0
 
-def get_dest_ip_counter(ip_dst_new,ip_dst_set,ip_dst_count):
 
-    if ip_dst_new not in ip_dst_set:
-        print ip_dst_set
-        ip_dst_count = ip_dst_count + 1
-        ip_dst_new = ip_dst_set
+
+def get_dest_ip_counter(L3_ip_dst_new):
+
+    global L3_ip_dst_counter
+
+    if L3_ip_dst_new not in L3_ip_dst_set:
+        L3_ip_dst_set.append(L3_ip_dst_new)
+        L3_ip_dst_counter = L3_ip_dst_counter + 1
     else:
-        ip_dst_new = ip_dst_set
+        pass
+        #print L3_ip_dst_set
 
-    return ip_dst_set,ip_dst_count 
+    return L3_ip_dst_set,L3_ip_dst_counter
 
-i=0
+
+i_counter=0
+L3_ip_dst_set = []
+L3_ip_dst_counter = 1
+
 # for filename in glob.iglob('captures_IoT_Sentinel/captures_IoT-Sentinel/**/*.pcap',recursive=True):
 #     print(filename)
 #f = open('captures_IoT_Sentinel/captures_IoT-Sentinel/HomeMaticPlug/Setup-C-4-STA.pcap')
@@ -71,7 +75,6 @@ f = open(str(sys.argv[1]))
 pcap = dpkt.pcap.Reader(f)
 
 
-
 for ts, buf in pcap:
 
 #Variables assignment
@@ -80,8 +83,6 @@ for ts, buf in pcap:
     L2_llc = 0
 
     L3_ip = 0
-    L3_ip_dst = ''
-    L3_ip_dst_count = 0
     L3_icmp = 0
     L3_icmp6 = 0
     L3_eapol = 0
@@ -109,21 +110,25 @@ for ts, buf in pcap:
     pck_rawdata = 'n/a'
 
     
-    i+=1
+    i_counter+=1
     
     #Assign ethernet buffer value to eth
     eth = dpkt.ethernet.Ethernet(buf)
     ip = eth.data
+
     
 
     #Network Layer IP
     if eth.type == dpkt.ethernet.ETH_TYPE_IP:
         L3_ip = 1
 
+        #Get packet size
         pck_size = len(ip.data)
-        ip_t=ip_to_str(ip.dst)
-        print ip_t
-        L3_ip_dst,L3_ip_dst_count=get_dest_ip_counter(ip_t,L3_ip_dst,L3_ip_dst_count)
+
+        #Check new destination IP
+        ip_dst_new=ip_to_str(ip.dst)
+        L3_ip_dst,L3_ip_dst_count=get_dest_ip_counter(ip_dst_new)
+
         print "DST count: ",L3_ip_dst_count
         print "DST IP: ",L3_ip_dst
 
@@ -135,6 +140,7 @@ for ts, buf in pcap:
             L3_icmp = 1
         if type(ip.data) == dpkt.icmp6.ICMP6:
             L3_icmp6 = 1
+    
     #Transport UDP DHCP-DNS-MDNS-SSDP-NTP
         if type(ip.data) == dpkt.udp.UDP:
             L4_udp = 1
@@ -152,6 +158,7 @@ for ts, buf in pcap:
                 L7_ssdp = 1
             if udp.sport == 123 or udp.dport == 123 :
                 L7_ntp = 1
+    
     #Transport TCP HTTP-HTTPS
         if type(ip.data) == dpkt.tcp.TCP:
             L4_tcp = 1
@@ -174,6 +181,7 @@ for ts, buf in pcap:
             L2_arp = 1            
         if eth.type == dpkt.llc.LLC:
             L2_llc= 1
+    
     #Network EAPoL
         if eth.type == dpkt.ethernet.ETH_TYPE_EAPOL:
             L3_eapol = 1
@@ -181,39 +189,10 @@ for ts, buf in pcap:
         print (i,'\n\nNon IP Packet type not supported  %s\n') % eth.data.__class__.__name__
         continue
 
-    # print "----------"
-    # print i
-    # #print 'ip_address_src= ',ip_to_str(ip.src) 
-    # print "L2 properties:"
-    # print "ARP: ",L2_arp
-    # print "LLC: ",L2_llc
-    # print "L3 properties:"
-    # print "EAPOL: ",L3_eapol
-    # print "IP: ",L3_ip
-    # print "IP packet size: ",pck_size
-    # print "IP raw data: ",pck_rawdata
-    # print "IP padding: ",ip_padding 
-    # print "IP Ralert: ",ip_ralert 
-    # print "IP add counter: ",ip_add_count 
-    # print "Port class src: ",port_class_src
-    # print "Port class dst: ",port_class_dst
-    # print "ICMP: ",L3_icmp
-    # print "ICMP6: ",L3_icmp6
-    # print "L4 properties:"
-    # print "TCP: ",L4_tcp
-    # print "UDP: ",L4_udp
-    # print "L7 properties:"
-    # print "HTTP: ",L7_http
-    # print "HTTPS: ",L7_https
-    # print "DHCP: ",L7_dhcp
-    # print "BOOTP: ",L7_bootp
-    # print "SSDP: ",L7_ssdp
-    # print "DNS: ",L7_dns
-    # print "MDNS: ",L7_mdns
-    # print "NTP: ",L7_ntp
+#Create the array containing the 23 features
 
-    ar = np.array([L2_arp,L2_llc,L3_eapol,L3_ip,pck_size,pck_rawdata,ip_padding,ip_ralert,ip_add_count,port_class_src,port_class_dst,L3_icmp,L3_icmp6,L4_tcp,L4_udp,L7_https,L7_dhcp,L7_bootp,L7_ssdp,L7_dns,L7_mdns,L7_ntp])
-    df = pandas.DataFrame(ar, columns = [i], index = ['arp', 'llc', 'eapol', 'ip','pck_size','pck_rawdata','ip_padding','ip_ralert','ip_add_count','portc_src','portc_dst','icmp','icmp6','tcp','udp','http','dhcp','bootp','ssdp','dns','mdns','ntp'])
+    ar = np.array([L2_arp,L2_llc,L3_eapol,L3_ip,pck_size,pck_rawdata,ip_padding,ip_ralert,L3_ip_dst_counter,port_class_src,port_class_dst,L3_icmp,L3_icmp6,L4_tcp,L4_udp,L7_https,L7_dhcp,L7_bootp,L7_ssdp,L7_dns,L7_mdns,L7_ntp])
+    df = pandas.DataFrame(ar, columns = [i_counter], index = ['arp', 'llc', 'eapol', 'ip','pck_size','pck_rawdata','ip_padding','ip_ralert','ip_add_count','portc_src','portc_dst','icmp','icmp6','tcp','udp','http','dhcp','bootp','ssdp','dns','mdns','ntp'])
     print (df)
     print ("\n")
 f.close()
